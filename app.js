11c6b865c3e02998e19823f6b2da2a1ba8a0b157
Samuel Amphoux <amphoux.s@hotmail.fr>
2024-03-20 08:55:14 +0100

    feat: add handler for routes outside of app scope

--- app.js
+++ app.js
@@ -13,10 +13,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -24,5 +20,12 @@ app.use((req, res, next) => {
 // Mounting Routers
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, _next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`,
+  });
+});
 
 module.exports = app;
