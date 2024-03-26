4c7546cab92d840416915dcbd66b8e54e364df5b
Samuel Amphoux <amphoux.s@hotmail.fr>
2024-03-26 11:22:31 +0100

    feat: add roles to users and restrict routes like delete to certain roles

--- controllers/authController.js
+++ controllers/authController.js
@@ -85,3 +85,15 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = currentUser;
   next();
 });
+
+exports.restrictTo =
+  (...roles) =>
+  (req, res, next) => {
+    // roles ['admin', 'lead-guide']
+    if (!roles.includes(req.user.role))
+      return next(
+        new AppError('You do not have permission to perform this action', 403),
+      );
+
+    next();
+  };
