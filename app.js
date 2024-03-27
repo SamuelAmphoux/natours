ed0eca83aa8fa6d2081a8f0e44871627f1e57f19
Samuel Amphoux <amphoux.s@hotmail.fr>
2024-03-27 11:12:16 +0100

    feat: add global middleware that limits rate to avoid security attacks such as DoS

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -9,8 +10,20 @@ const userRouter = require('./routes/userRoutes');
 // Initiating app
 const app = express();
 
-// Middlewares
+// Global Middlewares
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+const limiter = rateLimit({
+  // Allow 100 requests from same IP
+  max: 100,
+  // In 1 Hour
+  windowMs: 60 * 60 * 1000,
+  // Message sent when limit is exceeded
+  message: 'Too many requests from this IP, please try again in an hour',
+});
+// rateLimit creates a middleware that we can use on our app
+app.use('/api', limiter);
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
